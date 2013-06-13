57d2524fcd5669ef5b60b6adc216c406a5546661
Bastian Bartmann <xarfai27@gmail.com>
2013-06-13 16:04:35 +0200

    refactored subscribers_controller

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -5,58 +5,33 @@ class SubscribersController < StatusBoardWidgets
   get '/graph' do
     type = params[:type] || "line"
     feed_params = params.select { |k, v| k.include?("feed") }
-  
-    uri = URI.parse("http://api.uri.lv/feeds/subscribers.json")
-  
+    
     graph = {
       graph: {
         title: 'Subscriber',
         type: type,
         refreshEveryNSeconds: 3600,
-        datasequences: [
-    
-        ]
+        datasequences: []
       }
     }
   
     feed_params.each do |key, feed|
-      parameters = { :key => params[:api_key], :token => params[:token], :feed => feed }
-      uri.query = URI.encode_www_form(parameters)
-      stats = MultiJson.load(uri.open.read)["stats"].reverse
+      stats = fetch_subscribers(params[:api_key], params[:token], feed, true)
+      
       if feed_params.size == 1
         graph[:graph][:title] = feed.titleize.gsub('-', ' ')
-        greader = { title: "Google Reader", datapoints: [] }
-        stats.each do |entry|
-          greader[:datapoints] << {
-            title: Time.at(entry["day"]).strftime("%e.%-m."),
-            value: entry['greader']
-          }
-        end
+
+        greader = { title: "Google Reader", datapoints: create_datapoints(stats, 'greader') }
         graph[:graph][:datasequences] << greader
-        other = { title: "Other", datapoints: [] }
-        stats.each do |entry|
-          other[:datapoints] << {
-            title: Time.at(entry["day"]).strftime("%e.%-m."),
-            value: entry['other']
-          }
-        end
+
+        other = { title: "Other", datapoints: create_datapoints(stats, 'other') }
         graph[:graph][:datasequences] << other
-        direct = { title: "Direct", datapoints: [] }
-        stats.each do |entry|
-          direct[:datapoints] << {
-            title: Time.at(entry["day"]).strftime("%e.%-m."),
-            value: entry['direct']
-          }
-        end
+
+        direct = { title: "Direct", datapoints: create_datapoints(stats, 'direct') }
         graph[:graph][:datasequences] << direct
+      
       else
-        subscribers = { title: feed.titleize.gsub('-', ' '), datapoints: [] }
-        stats.each do |entry|
-          subscribers[:datapoints] << {
-            title: Time.at(entry["day"]).strftime("%e.%-m."),
-            value: entry['greader']
-          }
-        end
+        subscribers = { title: feed.titleize.gsub('-', ' '), datapoints: create_datapoints(stats, 'greader') }
         graph[:graph][:datasequences] << subscribers
       end
     end
@@ -64,30 +39,50 @@ class SubscribersController < StatusBoardWidgets
     json graph
   end
   
-  # Subscriber count with URI.LV
-  get '/count' do
-    feed_params = params.select { |k, v| k.include?("feed") }
-  
-    uri = URI.parse("http://api.uri.lv/feeds/subscribers.json")
-    feeds = []
-  
-    feed_params.each do |key, feed|
-      parameters = { :key => params[:api_key], :token => params[:token], :feed => feed }
-      uri.query = URI.encode_www_form(parameters)
-      stats = MultiJson.load(uri.open.read)["stats"].first
-      feeds << {
-        name: feed.titleize.gsub('-', ' '),
-        count: stats['greader']
-      }
-  
-    end
-  
-    @feeds = feeds.sort_by { |k| k[:count] }.reverse
-    erb :count
-  end
-  
+  # Subscriber table with URI.LV
   get '/table' do
     @uri = "/subscribers/count?" + URI.encode_www_form(params)
     erb :index
+  end
+
+  # Subscriber count with URI.LV
+  get '/count' do
+    @feeds = []
+    feed_params = params.select { |k, v| k.include?("feed") }
+  
+    feed_params.each do |key, feed|
+      stats = fetch_subscribers(params[:api_key], params[:token], feed)
+      @feeds << {
+        name: feed.titleize.gsub('-', ' '),
+        count: stats['greader']
+      }
+    end
+  
+    @feeds = @feeds.sort_by { |k| k[:count] }.reverse
+    erb :count
+  end
+
+  private
+
+  def fetch_subscribers(key, token, feed, history=false)
+    uri = URI.parse("http://api.uri.lv/feeds/subscribers.json")
+    parameters = { :key => key, :token => token, :feed => feed }
+    uri.query = URI.encode_www_form(parameters)
+    if history
+      MultiJson.load(uri.open.read)["stats"].reverse
+    else
+      MultiJson.load(uri.open.read)["stats"].first
+    end
+  end
+
+  def create_datapoints(stats, type)
+    datapoints = []
+    stats.each do |entry|
+      datapoints << {
+        title: Time.at(entry["day"]).strftime("%e.%-m."),
+        value: entry['greader']
+      }
+    end
+    datapoints
   end
 end
