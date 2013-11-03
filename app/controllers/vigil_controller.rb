b19f138fc8a1def4a330af888ab1b48a52746b02
coding-chimp <xarfai27@gmail.com>
2013-11-03 14:47:32 +0100

    rounded speed in vigil table

--- app/controllers/vigil_controller.rb
+++ app/controllers/vigil_controller.rb
@@ -5,23 +5,23 @@ class VigilController < StatusBoardWidgets
     @uri = "/vigil/table?" + URI.encode_www_form(params)
     erb :index
   end
-  
+
   get '/table' do
     @pages = []
-  
+
     @return = Nokogiri::HTML(open("http://api.vigil-app.com/v1/user/#{params[:user]}/host?populateHostMonitors&output=html"))
-  
+
     items = @return.css("tr")
     items.each do |item|
       name = item.at_css("td.name").text
       status = item.at_css("td.status-code").text
       time = item.at_css("td.total-time").text
-      speed = item.at_css("td.speed").text
-  
+      speed = item.at_css("td.speed").text.to_f.round(2)
+
       page = {name: name, status: status, time: time, speed: speed}
       @pages << page
     end
-  
+
     erb :table
   end
 end
